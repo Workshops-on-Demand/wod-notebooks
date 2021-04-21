57602b1a6915e2e5c21a6c4cbdec2eac6d8b69f4
jupyter <jupyter@notebooks.hpedev.io>
2021-04-21 10:08:34 +0200

    Preliminary version of a Docker WoD

---
+++ WKSHP-Docker101/config-db.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3b83e65f5d',
+  'passwordsalt' => '0c0de7daaf32263460afa3ec389852',
+  'secret' => 'b3860544f0c039627e7be309ff31ebaa2719edd6fdde1edaedc2b767ba0df4fde1e3350c53c32086cae229c0de3bb7c7',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => '/data/owncloud',
+  'overwrite.cli.url' => 'http://localhost/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '7.0.15',
+  'installed' => true,
+  'dbname' => 'owncloud',
+  'dbhost' => 'c6.labossi.hpintelco.org',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'owncloud',
+  'dbpassword' => 'owncloudpwd',
+);
---
+++ WKSHP-Docker101/owncloud/config.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3032c5405d',
+  'passwordsalt' => '3c4fe9cc8364534bf9d0fdf434dd94',
+  'secret' => 'ced743e9cc561107df9da9a5f329b877a339500d0aa1d53079ff823462fd4db0947b1685a9dc606d8f8a2718049145ca',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => '/data',
+  'overwrite.cli.url' => 'http://localhost/owncloud',
+  'dbtype' => 'sqlite3',
+  'version' => '7.0.6.2',
+  'installed' => true,
+);
