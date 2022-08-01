7ab44a243ef65f31b3cb5d660352738e98ec8ca5
jupyter <jupyter@notebooks.hpedev.io>
2022-08-01 17:48:53 +0200

    ez54 update for HPECP ML Pipeline

--- WKSHP-K8s-ML-Pipeline/Code/NYCTaxi/XGB_Scoringv2.py
+++ WKSHP-K8s-ML-Pipeline/Code/NYCTaxi/XGB_Scoringv2.py
@@ -27,7 +27,7 @@ def saveInProjectRepo(path):
 
 	# Loading the model back from the XGB pickle file
 final_gb = pickle.load(
-    open(saveInProjectRepo('models/NYCTaxi') + '/' + userID + '/' + "XGB.picklev2.dat", "rb"))
+    open(saveInProjectRepo('models') + '/' + userID + '/' + "XGB.picklev2.dat", "rb"))
 	
 # Loading encoding file
 # with open(saveInProjectRepo('data/encoding.json'), 'r') as file:
