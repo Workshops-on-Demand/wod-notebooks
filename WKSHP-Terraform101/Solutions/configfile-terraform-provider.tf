66ae472b14b11ff74114b104564905b07534b62d
Bruno Cornec <bruno@flossita.org>
2022-08-02 18:29:48 +0200

    Move some WoD as private

--- WKSHP-Terraform101/Solutions/configfile-terraform-provider.tf
+++
@@ -1,9 +0,0 @@
-# Load HPE GreenLake terraform provider
-terraform {
-      required_providers {
-         hpegl = {
-            source  = "hpe/hpegl"
-            version = "0.1.7"
-         }
-      }
-   }
